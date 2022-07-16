7d703b3fd1c29c61891a574719b17193c6a7a471
nbf-supplementary <109392889+nbf-supplementary@users.noreply.github.com>
2022-07-16 00:01:51 -0400

    Add files via upload

--- resources/vis.js
+++ resources/vis.js
@@ -15,15 +15,12 @@ camera.up = new THREE.Vector3(0, 0, 1);
 camera.position.y = -5;
 
 const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(960, 640);
 renderer.setSize(width, height);
-// document.body.appendChild(renderer.domElement);
 document.getElementById("canvas").appendChild(renderer.domElement);
 
 
 // Set up Controls
 const controls = new OrbitControls(camera, renderer.domElement, scene);
-// controls.setGizmosVisible(false);
 
 
 // Set up Lights
@@ -32,139 +29,163 @@ scene.add(light);
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
+const pointMats = [];
 
-// Load Convexes
-const cvxGroup = new THREE.Group();
-scene.add(cvxGroup);
 
-const cvxs = [];
-const cvxLoader = new OBJLoader();
-function onLoadConvex(obj, i) {
-    const subgroup = new THREE.Group();
-    const geometry = obj.children[0].geometry;
-    const material = new THREE.MeshBasicMaterial({ color: 0x404040 });
-    material.transparent = true;
-    material.opacity = 0.2;
-    const cvx = new THREE.Mesh(geometry, material);
-    subgroup.add(cvx);
+function loadScene(root, instCount, visible) {
+    const sceneGroup = new THREE.Group();
+    sceneGroup.visible = visible;
 
-    const material2 = new THREE.MeshBasicMaterial({ color: 0x404040 });
-    material2.wireframe = true;
-    const cvx2 = new THREE.Mesh(geometry, material2);
-    subgroup.add(cvx2);
+    // Load Convexes
+    const cvxGroup = new THREE.Group();
+    sceneGroup.add(cvxGroup);
 
-    cvxGroup.add(subgroup);
-    cvxs[i] = subgroup;
-    if (i != 0) {
-        subgroup.visible = false;
-    } else {
+    const cvxs = [];
+    const cvxLoader = new OBJLoader();
+    function onLoadConvex(obj, i) {
+        const subgroup = new THREE.Group();
+        const geometry = obj.children[0].geometry;
+        const material = new THREE.MeshBasicMaterial({ color: 0x404040 });
+        material.transparent = true;
+        material.opacity = 0.2;
+        const cvx = new THREE.Mesh(geometry, material);
+        subgroup.add(cvx);
+
+        const material2 = new THREE.MeshBasicMaterial({ color: 0x404040 });
+        material2.wireframe = true;
+        const cvx2 = new THREE.Mesh(geometry, material2);
+        subgroup.add(cvx2);
+
+        cvxGroup.add(subgroup);
+        cvxs[i] = subgroup;
+        if (i != 0) {
+            subgroup.visible = false;
+        } else {
+            renderer.render(scene, camera);
+        }
+    }
+    for (var i = 0; i < instCount; i++) {
+        cvxs.push(null);
+        const id = i;
+        cvxLoader.load(root + "/cvx_hulls/" + i + ".obj", function (o) {
+            onLoadConvex(o, id);
+        });
+    }
+
+
+    // Load points
+    const plyLoader = new PLYLoader();
+
+    const seedGroup = new THREE.Group();
+    sceneGroup.add(seedGroup);
+    plyLoader.load(root + "/scene.seeds.ply", function (geometry) {
+        const material = new THREE.PointsMaterial({ color: 0xff0000 });
+        material.size = 0.1;
+        const points = new THREE.Points(geometry, material);
+        seedGroup.add(points);
         renderer.render(scene, camera);
+    });
+
+    const instGroup = new THREE.Group();
+    sceneGroup.add(instGroup);
+    plyLoader.load(root + "/scene.pts.instance_pred.ply", function (geometry) {
+        const material = new THREE.PointsMaterial({ vertexColors: true });
+        material.size = 0.01;
+        pointMats.push(material);
+        const points = new THREE.Points(geometry, material);
+        instGroup.add(points);
+        renderer.render(scene, camera);
+    });
+
+    const inpGroup = new THREE.Group();
+    inpGroup.visible = false;
+    sceneGroup.add(inpGroup);
+    plyLoader.load(root + "/scene.pts.input.ply", function (geometry) {
+        const material = new THREE.PointsMaterial({ color: 0x303030 });
+        material.size = 0.01;
+        pointMats.push(material);
+        const points = new THREE.Points(geometry, material);
+        inpGroup.add(points);
+        renderer.render(scene, camera);
+    });
+
+    scene.add(sceneGroup);
+
+    return {
+        sceneGroup: sceneGroup,
+        inpGroup: inpGroup,
+        seedGroup: seedGroup,
+        instGroup: instGroup,
+        cvxGroup: cvxGroup,
+        cvxs: cvxs,
     }
 }
-for (var i = 0; i < 14; i++) {
-    cvxs.push(null);
-    const id = i;
-    cvxLoader.load("resources/scene/cvx_hulls/0_12_" + i + ".obj", function (o) {
-        onLoadConvex(o, id);
-    });
+
+const scenesData = [
+    loadScene("resources/scene0", 23, true),
+    loadScene("resources/scene1", 31, false),
+    loadScene("resources/scene2", 14, false),
+    loadScene("resources/scene3", 16, false),
+    loadScene("resources/scene4", 33, false),
+    loadScene("resources/scene5", 15, false),
+    loadScene("resources/scene6", 15, false),
+];
+
+function removeLoadMsg() {
+    document.getElementById("load-warning").style.visibility = "hidden";
+    renderer.render(scene, camera);
 }
-
-
-// Load points
-const plyLoader = new PLYLoader();
-
-const seedGroup = new THREE.Group();
-scene.add(seedGroup);
-plyLoader.load("resources/scene/scene0046_01.seeds.ply", function (geometry) {
-    const material = new THREE.PointsMaterial({ color: 0xff0000 });
-    material.size = 0.1;
-    const points = new THREE.Points(geometry, material);
-    seedGroup.add(points);
-    renderer.render(scene, camera);
-});
-
-const instGroup = new THREE.Group();
-scene.add(instGroup);
-plyLoader.load("resources/scene/scene0046_01.pts.instance_pred.ply", function (geometry) {
-    const material = new THREE.PointsMaterial({ vertexColors: true });
-    material.size = 0.01;
-    const points = new THREE.Points(geometry, material);
-    instGroup.add(points);
-    renderer.render(scene, camera);
-});
-
-const gtGroup = new THREE.Group();
-gtGroup.visible = false;
-scene.add(gtGroup);
-plyLoader.load("resources/scene/scene0046_01.pts.instance_gt.ply", function (geometry) {
-    const material = new THREE.PointsMaterial({ vertexColors: true });
-    material.size = 0.01;
-    const points = new THREE.Points(geometry, material);
-    gtGroup.add(points);
-});
-
-const inpGroup = new THREE.Group();
-inpGroup.visible = false;
-scene.add(inpGroup);
-plyLoader.load("resources/scene/scene0046_01.pts.input.ply", function (geometry) {
-    const material = new THREE.PointsMaterial({ color: 0x303030 });
-    material.size = 0.01;
-    const points = new THREE.Points(geometry, material);
-    inpGroup.add(points);
-});
+setTimeout(removeLoadMsg, 15000.0); // This is definitely how loading works, trust me.
 
 
 // Set up GUI
 const gui = new GUI();
-gui.onFinishChange(function () {
-    if (guiState.showCvx) {
-        cvxGroup.visible = true;
-    } else {
-        cvxGroup.visible = false;
+gui.onChange(function () {
+    scenesData.forEach(function (sd) {
+        if (guiState.showSeeds) {
+            sd.seedGroup.visible = true;
+        } else {
+            sd.seedGroup.visible = false;
+        }
+        if (guiState.showInst) {
+            sd.instGroup.visible = true;
+            sd.inpGroup.visible = false;
+        } else {
+            sd.instGroup.visible = false;
+            sd.inpGroup.visible = true;
+        }
+        for (var i = 0; i < sd.cvxs.length; i++) {
+            sd.cvxs[i].visible = (i == guiState.currentInst) || guiState.showCvx;
+        }
+    });
+
+    for (var i = 0; i < scenesData.length; i++) {
+        scenesData[i].sceneGroup.visible = (i == guiState.currentScene);
     }
-    if (guiState.showSeeds) {
-        seedGroup.visible = true;
-    } else {
-        seedGroup.visible = false;
-    }
-    if (guiState.layer == "Instance Segmentation") {
-        instGroup.visible = true;
-        gtGroup.visible = false;
-        inpGroup.visible = false;
-    } else if (guiState.layer == "Instance Segmentation (Ground Truth)") {
-        instGroup.visible = false;
-        gtGroup.visible = true;
-        inpGroup.visible = false;
-    } else if (guiState.layer == "Input Points") {
-        instGroup.visible = false;
-        gtGroup.visible = false;
-        inpGroup.visible = true;
-    }
-    for (var i = 0; i < cvxs.length; i++) {
-        cvxs[i].visible = (i == guiState.currentInst);
-    }
+
+    pointMats.forEach(function (mat) {
+        mat.size = guiState.pointSize;
+    });
 
     renderer.render(scene, camera);
 });
 
 const guiState = {
-    layer: "Instance Segmentation",
-    showCvx: true,
+    showInst: true,
+    showCvx: false,
     showSeeds: true,
+    currentScene: 0,
     currentInst: 0,
+    pointSize: 0.01,
 };
 
-gui.add(guiState, "showCvx");
-gui.add(guiState, "showSeeds");
-const instIDs = [];
-for (var i = 0; i < cvxs.length; i++) instIDs.push(i);
-gui.add(guiState, "currentInst", instIDs);
-gui.add(guiState, 'layer', [
-    "Input Points",
-    "Instance Segmentation",
-    // "Instance Segmentation (Ground Truth)",
-    // "Convex Hulls"
-]);
+gui.add(guiState, "showCvx").name("Show All Instances");
+gui.add(guiState, "showInst").name("Show Instance Segmentation");
+gui.add(guiState, "showSeeds").name("Show Queries");
+gui.add(guiState, "pointSize", 0.005, 0.05).name("Point Scale");
+gui.add(guiState, "currentInst", 0, 33, 1).name("Instance ID");
+const sceneIDs = []; for (var i = 0; i < scenesData.length; i++) sceneIDs.push(i);
+gui.add(guiState, "currentScene", sceneIDs).name("Scene");
 
 controls.addEventListener("change", function () {
     renderer.render(scene, camera);
